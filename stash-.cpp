1894b6e9f5128df7e1a1194cf760a333103cb9c6
ekko <953472374@qq.com>
2023-06-09 17:38:38 +0800

    cpp
    	new file:   stash-.cpp

---
+++ stash-.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// 辗转相除法求最大公因数
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+// 计算最小公倍数
+int lcm(int a, int b)
+{
+    return a * b / gcd(a, b);
+}
+
+int main()
+{
+    int a, b;
+    cout << "请输入两个正整数：";
+    cin >> a >> b;
+    cout << "最大公因数为：" << gcd(a, b) << endl;
+    cout << "最小公倍数为：" << lcm(a, b) << endl;
+    return 0;
+}
